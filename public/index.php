b4ba1678fda27678166881185a1e374d6c498d42
Ciurca Alina <ciurca.alina@gmail.com>
2021-06-15 01:06:07 +0300

    upload style changes

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Curriculum Vitae of Ciurca Alina</title>
+</head>
+<body>
+       <h1>Ciurca Alina</h1>
+       <p>email:ciurca.alina@gmail.com</p>
+       <div>telefon: 3732256898</>
+       <br>
+       <div>
+        <?php echo "Today is " . date("Y/m/d H:i:s"); ?>
+    </div>
+    </br>
+</body>
+</html>
